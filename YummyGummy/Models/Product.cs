a57fb2f74d8bd84d07ae68e8aa8a14b0e38b36ff
DaveKOregon <david.karstens@fidgetech.org>
2018-01-07 15:13:34 -0800

    Added Product model

---
+++ YummyGummy/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace YummyGummy.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public string ImageURL { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ YummyGummy/Models/YummyGummyDBContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace YummyGummy.Models
+{
+    public class YummyGummyDBContext : DbContext
+    {
+        public YummyGummyDBContext()
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(@"Server=localhost;Port=3006;database=YummyGummy;uid=root;pwd=root;");
+        }
+
+        public YummyGummyDBContext(DbContextOptions<YummyGummyDBContext> options)
+            :base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
